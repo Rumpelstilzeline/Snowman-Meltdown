d0c9c06c4a1d4ebc51dfb31c641bb1702b925bcd
Rumpelstilzeline <schwab.julia.jessica@gmail.com>
2025-06-06 10:32:20 +0200

    added STAGES, display_game_state function and improved game loop (checking if 'guess' is in 'secret_word') and printing current stage of game)

--- snowman.py
+++ snowman.py
@@ -1,5 +1,35 @@
 import random
 
+# Snowman ASCII Art stages
+STAGES = [
+     # Stage 0: Full snowman
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     ( : ) 
+     ( : ) 
+     """,
+     # Stage 1: Bottom part starts melting
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     ( : ) 
+     """,
+     # Stage 2: Only the head remains
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     """,
+     # Stage 3: Snowman completely melted
+     """
+      ___  
+     /___\\ 
+     """
+ ]
+
 # List of secret words
 WORDS = ["python", "git", "github", "snowman", "meltdown"]
 
@@ -8,19 +38,35 @@ def get_random_word():
     """Selects a random word from the list."""
     return WORDS[random.randint(0, len(WORDS) - 1)]
 
+def display_game_state(mistakes, secret_word, guessed_letters):
+    if mistakes == 0:
+        print(STAGES[1])
+    elif mistakes == 1:
+        print(STAGES[2])
+    elif mistakes == 2:
+        print(STAGES[3])
+
 
 def play_game():
     secret_word = get_random_word()
     print("Welcome to Snowman Meltdown!")
+    print(STAGES[0])
+    for letter in secret_word:
+        print(letter, end='')
     print("Secret word selected: " + secret_word)  # for testing, later remove this line
 
     # Game Loop
     mistakes = 0
+    guessed_letters = []
     bad_input = True
     while mistakes < 3:
         guess = input("Guess a letter: ").lower()
         print("You guessed:", guess)
-        mistakes += 1
+        if guess in secret_word:
+            guessed_letters.add(guess)
+        display_game_state(mistakes, secret_word, guessed_letters)
+        if guess not in secret_word:
+            mistakes += 1
 
 
 if __name__ == "__main__":
